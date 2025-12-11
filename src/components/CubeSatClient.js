b4e3bd5c0aa7736836eb0bd48ec85c14a6c74457
Eduardo Barata <eduardo.barata@tecnico.ulisboa.pt>
2025-12-11 22:24:33 +0000

    CubeSat in gif format

--- src/components/CubeSatClient.js
+++ src/components/CubeSatClient.js
@@ -1,6 +1,8 @@
 "use client"
 
 import dynamic from "next/dynamic"
+import { useState, useEffect } from "react"
+import Image from "next/image"
 
 const CubeSatViewer = dynamic(() => import("./CubeSat"), {
   ssr: false,
@@ -12,9 +14,22 @@ const CubeSatViewer = dynamic(() => import("./CubeSat"), {
 })
 
 export default function CubeSatClient() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+    
+    checkMobile()
+    window.addEventListener('resize', checkMobile)
+    
+    return () => window.removeEventListener('resize', checkMobile)
+  }, [])
+
   return (
     <div 
-      className="w-[400px] h-[400px] md:w-[500px] md:h-[500px]"
+      className="w-[500px] h-[500px] md:w-[700px] md:h-[700px]"
       style={{
         position: 'relative',
         transform: 'translateZ(0)',
@@ -24,7 +39,21 @@ export default function CubeSatClient() {
         willChange: 'auto'
       }}
     >
-      <CubeSatViewer />
+      {true ? ( //isMobile ? ( -> For now always show gif on all devices
+        <div className="w-full h-full flex items-center justify-center">
+          <Image
+            src="/cubesat-render/cubesat.gif"
+            alt="CubeSat rotating animation"
+            width={1920}
+            height={1080}
+            className="w-full h-full object-contain"
+            priority
+            unoptimized
+          />
+        </div>
+      ) : (
+        <CubeSatViewer />
+      )}
     </div>
   )
 }
